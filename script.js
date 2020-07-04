61ffc20f289f496760f2318b95dc8b592ba1a189
rmazurek1 <radoslawmazurek76@gmail.com>
2020-07-04 18:43:14 +0200

    Finished hero section and about Alex

---
+++ script.js
@@ -0,0 +1,11 @@
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.navigation');
+
+
+const handleClick = () => {
+    hamburger.classList.toggle('hamburger--active');
+    nav.classList.toggle('navigation--active');
+}
+
+
+hamburger.addEventListener('click', handleClick);
