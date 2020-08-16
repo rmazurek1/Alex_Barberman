87ced7fa6b45df123a64c0ad3d1681f2cfc635d6
rmazurek1 <radoslawmazurek76@gmail.com>
2020-08-16 15:27:19 +0200

    add map API and some features

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ const handleClick = () => {
 
 
 hamburger.addEventListener('click', handleClick);
+
+function initMap() {
+    // The location of Uluru
+    var uluru = {
+        lat: 51.112288,
+        lng: 17.013194
+    };
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 15,
+            center: uluru
+        });
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+
+}
